9497f737b4a341f3a40c4ccbb5c87f18770316ec
kentaro <ks.1984ever@gmail.com>
2018-01-06 09:40:03 +0000

    blog.rb validate

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
    has_many :favorites, dependent: :destroy
    has_many :favorite_users, through: :favorites, source: :user
    belongs_to :user
+   
+   validates :title,  presence: true
+   validates :content, presence: true
 end
